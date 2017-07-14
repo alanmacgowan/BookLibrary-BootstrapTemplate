35551d5b220d340c93aa37738f80f7fb632d0598
alanmacgowan <alanmacgowan@gmail.com>
2017-07-14 00:10:48 -0300

    added webpack dev server

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,6 +2,8 @@ var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.common.js');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./helpers');
+var path = require('path');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
@@ -13,7 +15,15 @@ module.exports = webpackMerge(commonConfig, {
     chunkFilename: '[id].chunk.js'
   },
   plugins: [
-    new ExtractTextPlugin('[name].css')
+    new ExtractTextPlugin('[name].css'),
+    //new webpack.HotModuleReplacementPlugin(),
+    new OpenBrowserPlugin({ url: 'http://localhost:8080/login.html' })
   ],
-
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    historyApiFallback: true,
+    quiet: true,
+    inline: true,
+    stats: 'minimal' // none (or false), errors-only, minimal, normal (or true) and verbose
+  }
 });
